acac87dc47972f013997c12fef84b72f4a9833a9
Yash Agrahari <yashagrahari456@gmail.com>
2024-06-13 01:13:50 +0530

    added auth functionality and managed state with React Redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import PrivateRoute from "./components/PrivateRoute"
+import Profile from "./pages/Profile"
+import About from "./pages/About"
 
 export default function App() {
   return (
-    <>
-      <Header/>
-    </>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/about" element={<About/>} />
+      <Route element={<PrivateRoute/>}>
+        <Route path="/profile" element={<Profile/>} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
  
